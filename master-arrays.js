546872ee3c9a1fea88bcd1b0607c5b1d28077049
aisisgallardo <83318557+aisisgallardo@users.noreply.github.com>
2022-12-28 00:46:43 -0500

    propose repondes
    28/12/2022

--- master-arrays.js
+++ master-arrays.js
@@ -7,10 +7,10 @@
   2 - from the provided array of persons console log how many of
   them have a name starting with the letter l 
 
-  3 - from the provided array persons create a new array that has 
+  3 - from the provided array persons creat a new array that has 
         the following properties
         {
-            name:"one name",
+            name:"name",
             canDrive:true|false,
         }
 
@@ -19,3 +19,21 @@
 */
 
 const persons = [{name:"lulu",age:20},{name:"leila",age:5},{name:"jose",age:10}];
+persons.forEach(function (person) {
+      console.log(person);
+})
+
+
+persons.filter((person) => person.name[0]==='l').length;
+
+persons.map(person => ({
+
+    name:person.name,
+    canDrive:person.age >= 18 
+
+}))
+
+console.log(
+    persons.reduce((youngest,person)=> person.age<=youngest.age?person:youngest, {age:300})   
+
+)
